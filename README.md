c92039b76991ffc922708d53eecf988789e9ac0f
Oleg Lavrovsky <oleg@utou.ch>
2018-12-06 21:35:22 +0100

    Initial package :tada:

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a Data Package with an extract of data provided by the Swiss film archive [Lichtspiel](http://www.lichtspiel.ch/) in Bern, Switzerland. You can use the existing data on the web-based search page at [lichtspiel.ch/en/datenbank/film](http://www.lichtspiel.ch/en/datenbank/film/)
+
+# Source
+
+An XML formatted export from the database was obtained from David Landolf for the [OpenGLAM 2018](http://openglam.ch/) hackathon. In the `src` folder you will find our conversion script, and a sample XML and JSON.
+
+# License
+
+The licensing terms of this data have not yet been established. If you intended to use these data in a public or commercial product, check with each of the data sources for any specific restrictions.
+
+This README and other metadata contained in this Data Package is licensed by its maintainers under the [Public Domain Dedication and License](https://opendatacommons.org/licenses/pddl/1-0/).
---
+++ src/convert.py
@@ -0,0 +1,57 @@
+from csv import DictWriter, QUOTE_MINIMAL
+from urllib.parse import urlencode
+from lxml import etree
+
+FILENAME = "MIDAS_Export_26102018.xml"
+
+OUTPUT = "lichtspiel.csv"
+
+SCHEMA = {
+	'id':		None, 
+	'copy_id':	None,
+	'title':	'./Titles/Title',
+	'country':	'./CountryOfOrigin',
+	'year':		'./YearOfProduction',
+	'type':		'./FilmType',
+	'length':  	'./OriginalLength',
+	'crew':		'./CrewCast/Person/Name',
+	'text_de': 	'./ContentDescription/Content/[@Language="default"]',
+	'text_en': 	'./ContentDescription/Content/[@Language="Englisch"]',
+}
+
+MAX_ROWS = 5
+
+
+#import xmltodict, json
+#with open(FILENAME, 'r') as data:
+	#o = xmltodict.parse(data.read())
+	#print(json.dumps(o))
+
+
+tree = etree.parse(FILENAME)
+
+with open(OUTPUT, 'w+') as csvout:
+	writer = DictWriter(csvout, fieldnames=SCHEMA.keys(),
+		delimiter=',', quotechar='"', quoting=QUOTE_MINIMAL)
+	writer.writeheader()
+		
+	for es in tree.findall('ExportFilms/Film')[0:MAX_ROWS]:
+		obj = SCHEMA.copy()
+		for k in obj:
+			if obj[k] is None: continue
+			n = es.find(obj[k])
+			if n is None: 
+				obj[k] = "" 
+			else:
+				obj[k] = ' '.join(n.text.strip()[0:100].splitlines())
+				if len(n.text) > len(obj[k])+10: obj[k] = obj[k] + ' ...'
+				if obj[k].isdigit(): obj[k] = int(obj[k])
+		
+		obj['id'] = int(es.get('ContentID'))
+		
+		copies = es.find('./Copies/Film')
+		obj['copy_id'] = 0
+		if copies is not None: obj['copy_id'] = int(copies.get('CopyID'))
+		
+		writer.writerow(obj)
+		
